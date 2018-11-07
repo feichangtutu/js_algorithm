7f8c0c3e2218d67fb1039a90f3fbf326b5826b14
Yanni <jiayanni2008@gmail.com>
2018-11-07 11:17:58 +0800

    Update deepClone.js

--- deepClone.js
+++ deepClone.js
@@ -1,3 +1,10 @@
 function deepClone(obj){
-	
+	let newObj = {}
+	for(let key in obj){
+		newObj[key] = obj[key]
+		if(typeof(obj[key])=='object'){
+			deepClone(obj[key])
+		}
+	}
+	return newObj
 }
